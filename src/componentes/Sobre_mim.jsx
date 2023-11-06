8272c58563b2001efb9b7ccae921804a53705094
Denivan Oliveira <denyoliver777@gmail.com>
2023-11-06 19:35:53 -0300

    feat: antigo portfolio

---
+++ src/componentes/Sobre_mim.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+export default function Sobre_mim() {
+    return (
+        <div>
+            <section id='sobremim'>
+                <h1>
+                    Sobre mim
+                </h1>
+                <p>
+                    Me chamo <strong>Denivan Oliveira dos Santos</strong>, <br />
+                    Como podem ver sou desenvolvedor web front-end. <br />
+                    Tambem trabalho com as ferramentas do CorelDraw e Photoshop. <br />
+                    Com tudo, sou um apaixonado por artes, esportes e por tecnoligias .
+                </p>
+                <p>
+                    A arte de pintar, desenhar, de imaginar <br />
+                    é onde eu me escondo, <br />
+                    onde tristesas e alegrias se misturam.
+                </p>
+                <div id='esporte'>
+                </div>
+                <p>
+                    Como programador sonho em fazer parte de grandes projetos, <br />
+                    sabe aquela sensação boa, de olhar para algo finalizado <br />
+                    e saber que você fez parte, saber que todo o seu esforço não foi em vão, <br />
+                    é uma sensação maravilhosa. <br />
+                    Esse sou eu, ou melhor um pouco de quem eu sou.
+                </p>
+                <p>
+                    Espero que minha trajetória possa fazer parte de seus projetos, <br />
+                    para isso busco a cada dia mais conhecimento, <br />
+                    para que o meu sonho possa ser realizado atravez do seu.
+                </p>
+            </section>
+        </div>
+    )
+}
